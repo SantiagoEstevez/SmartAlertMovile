ef99599c0748162f5963da24904673006512ee2a
Santiago Pako <santiago.estevez.m@gmail.com>
2018-06-01 23:03:03 -0300

    se agrega grafica pie

--- app/src/main/java/com/santiago/smartalert/views/NodeDetailFragment.java
+++ app/src/main/java/com/santiago/smartalert/views/NodeDetailFragment.java
@@ -1,6 +1,7 @@
 package com.santiago.smartalert.views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -9,14 +10,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.santiago.smartalert.R;
+
+import java.util.ArrayList;
 
 public class NodeDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Log.e("ggtets", "estoy en el detalle y vine de: " + getArguments().getString("nodeName"));
-        return inflater.inflate(R.layout.fragment_node_detail, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_node_detail, container, false);
+        PieChart chart = (PieChart) rootView.findViewById(R.id.chart);
+        chart.setUsePercentValues(true);
+        chart.getDescription().setEnabled(false);
+
+        chart.setDragDecelerationFrictionCoef(0.15f);
+
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleColor(Color.WHITE);
+        chart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(34f, "exto1"));
+        yValues.add(new PieEntry(23f, "exto2"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Memoria");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        chart.setData(data);
+
+
+        return rootView;
     }
 }
